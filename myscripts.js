b1fed12ebf953fe16777306d0a4dc99fa00890a0
Pontusbolmer <ponteeee@live.se>
2017-11-15 12:59:37 +0000

    Jag sitter fast

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,17 @@
-console.log("YELLO");
+$(document).ready(function() {
+    sessionStorage.userName = "test";
+    sessionStorage.pass = "password"
+
+  
+    
+    $("#login").click(function()
+    {
+        $(".userName").text(sessionStorage.userName);
+        $(".pass").text(sessionStorage.pass);
+    });    
+    
+    
+ 
+ 
+
+});
